05cc6101942d1dafe59e4da6827666ba3b42bce6
309 <309@academicos.cesde.edu.co>
2023-03-18 11:31:43 -0500

    confirmacion de contraseña

--- while.py
+++ while.py
@@ -6,11 +6,26 @@
 while i <= 15 :
     print(i)
     i +=1
-print("ciclo while termino")"""
+print("ciclo while termino")
 
 num = 0
 num2 = int(input("ingrese el limite de rango: "))
 while num < num2:
     num +=1
     if num % 2 == 0:
-        print(num) 
+        print(num) """
+        
+
+#programa que solicite confirmar contraseña 
+
+contraseña = input("ingrese una contraseña: ")
+
+confirmacion = input("ingrese la clave de confirmacion: ")
+
+while contraseña != confirmacion :
+    print("su contraeña es incorrecta ingrese de nuevo")
+    contraseña = input("ingrese una contraseña: ")
+    confirmacion = input("ingrese la clave de confirmacion: ")
+print("validacion confirmada")
+
+    
